26144ff59ee4f932c31a67a256d71d440f2dcd18
James Kerrane <hi@thatrobot.dev>
2020-03-04 05:07:46 +0000

    Format code

--- main.py
+++ main.py
@@ -9,7 +9,8 @@
 
 import random
 
-class bcolors: # colors for console text
+
+class bcolors:  # colors for console text
     PURPLE = '\033[95m'
     BLUE = '\033[94m'
     GREEN = '\033[92m'
@@ -17,6 +18,7 @@ class bcolors: # colors for console text
     UNDERLINE = '\033[4m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
+
 
 gameCount = 0
 while True:
@@ -37,80 +39,111 @@ Be careful! Many perils await.
 
     mobNum = random.randrange(1, 10)
 
-    singularNiceMobNames = ["chicken", "horse", "cow", "sheep", "seahorse", "pig"]
-    pluralNiceMobNames = ["chickens", "horses", "cows", "sheep", "seahorses", "sheep"]
+    singularNiceMobNames = [
+        "chicken", "horse", "cow", "sheep", "seahorse", "pig"
+    ]
+    pluralNiceMobNames = [
+        "chickens", "horses", "cows", "sheep", "seahorses", "sheep"
+    ]
     colors = ["purple", "rainbow", "gray", "brown", "white", "green"]
 
     if mobNum > 1:
-      mob = pluralNiceMobNames[random.randrange(len(pluralNiceMobNames))]
-      pluralMob = True
+        mob = pluralNiceMobNames[random.randrange(len(pluralNiceMobNames))]
+        pluralMob = True
     else:
-      mob = singularNiceMobNames[random.randrange(len(singularNiceMobNames))]
-      pluralMob = False
+        mob = singularNiceMobNames[random.randrange(len(singularNiceMobNames))]
+        pluralMob = False
 
     if mob == "sheep":
-      sheepColor = colors[random.randrange(len(colors))]
-      mob = sheepColor + " " + mob
+        sheepColor = colors[random.randrange(len(colors))]
+        mob = sheepColor + " " + mob
 
-    print("You've spawned in the world, and you see " + bcolors.ORANGE + str(mobNum) + " " + str(mob) + bcolors.ENDC + ".")
+    print("You've spawned in the world, and you see " + bcolors.ORANGE +
+          str(mobNum) + " " + str(mob) + bcolors.ENDC + ".")
 
     killedInnocentAnimalorAnimalsThatIsorAreProbablyStarvingOrHadAVeryHardLifeOrSomething = None
 
-    while killedInnocentAnimalorAnimalsThatIsorAreProbablyStarvingOrHadAVeryHardLifeOrSomething not in {"y", "n"}:
-        killedInnocentAnimalorAnimalsThatIsorAreProbablyStarvingOrHadAVeryHardLifeOrSomething = input(bcolors.BLUE + "\nStart punching them? (y/n) " + bcolors.PURPLE)
+    while killedInnocentAnimalorAnimalsThatIsorAreProbablyStarvingOrHadAVeryHardLifeOrSomething not in {
+            "y", "n"
+    }:
+        killedInnocentAnimalorAnimalsThatIsorAreProbablyStarvingOrHadAVeryHardLifeOrSomething = input(
+            bcolors.BLUE + "\nStart punching them? (y/n) " + bcolors.PURPLE)
 
-        killedInnocentAnimalorAnimalsThatIsorAreProbablyStarvingOrHadAVeryHardLifeOrSomething.lower()
+        killedInnocentAnimalorAnimalsThatIsorAreProbablyStarvingOrHadAVeryHardLifeOrSomething.lower(
+        )
 
         print(bcolors.ENDC)
 
     killedAnimals = False
 
     if killedInnocentAnimalorAnimalsThatIsorAreProbablyStarvingOrHadAVeryHardLifeOrSomething == "y":
-      print("Wow, your first act of life in this new world is to start killing inncoent animals. PETA would be proud.")
-      print(bcolors.UNDERLINE + bcolors.ORANGE + "\nThe " + mob + bcolors.ENDC + bcolors.UNDERLINE + " fall", end = '')
+        print(
+            "Wow, your first act of life in this new world is to start killing inncoent animals. PETA would be proud."
+        )
+        print(
+            bcolors.UNDERLINE + bcolors.ORANGE + "\nThe " + mob + bcolors.ENDC
+            + bcolors.UNDERLINE + " fall",
+            end='')
 
-      if pluralMob == False:
-        print("s", end = '')
-      print(" on the ground with a hard thud.\n" + bcolors.ENDC)
-        
-      killedAnimals = True
+        if pluralMob == False:
+            print("s", end='')
+        print(" on the ground with a hard thud.\n" + bcolors.ENDC)
+
+        killedAnimals = True
     elif killedInnocentAnimalorAnimalsThatIsorAreProbablyStarvingOrHadAVeryHardLifeOrSomething == "n":
-      print("Good you're not a monster.\n")
+        print("Good you're not a monster.\n")
 
-
-    print("You continue through the world, ", end = '')
-
+    print("You continue through the world, ", end='')
 
     if killedAnimals == False:
-      print("eager to enjoy more of the wildlife, when you realize ", end = '')
+        print("eager to enjoy more of the wildlife, when you realize ", end='')
     else:
-      print(bcolors.ORANGE + "looking for things occupy yourself other than killing animals, when you realize " + bcolors.ENDC, end = '')
+        print(
+            bcolors.ORANGE +
+            "looking for things occupy yourself other than killing animals, when you realize "
+            + bcolors.ENDC,
+            end='')
 
-    print("the sun is already setting! You need to chop down some wood from the forest nearby, so you can build yourself a house to protect yourself from the monsters that come out at night!")
+    print(
+        "the sun is already setting! You need to chop down some wood from the forest nearby, so you can build yourself a house to protect yourself from the monsters that come out at night!"
+    )
 
     forestType = None
 
     while forestType not in {"1", "2", "3"}:
-      forestType = input(bcolors.BLUE + "\nWhich forest would you like to chop wood from? The birch forest (1), spruce forest (2), or the cave (3). " + bcolors.PURPLE)
-      print(bcolors.ENDC, end = '')
+        forestType = input(
+            bcolors.BLUE +
+            "\nWhich forest would you like to chop wood from? The birch forest (1), spruce forest (2), or the cave (3). "
+            + bcolors.PURPLE)
+        print(bcolors.ENDC, end='')
 
     forests = ["birch", "spruce", "cave"]
     forest = forests[int(forestType) - 1]
     if forest == "cave":
-      print(bcolors.FAIL + "Y'all, that's not a forest. That's a cave. You go into the cave, shockingly underprepared for the monsters within. You get hit in the head with an arrow from a skeleton and die. You did not survive your first night of Minceraft." + bcolors.ENDC)
+        print(
+            bcolors.FAIL +
+            "Y'all, that's not a forest. That's a cave. You go into the cave, shockingly underprepared for the monsters within. You get hit in the head with an arrow from a skeleton and die. You did not survive your first night of Minceraft."
+            + bcolors.ENDC)
     else:
-      print("\nYou create a", end = '')
-      if forest == "birch":
-        print("n albeit, UGLY ", end = '')
-      print(bcolors.ORANGE + forest + " house" + bcolors.ENDC + ", and hide through the night. No creepers knocking on your door tonight! :D")
-      print(bcolors.GREEN + "\nYou've survived your first night of Mincecraft! Congratulations! Try again for a different ending." + bcolors.ENDC)
+        print("\nYou create a", end='')
+        if forest == "birch":
+            print("n albeit, UGLY ", end='')
+        print(
+            bcolors.ORANGE + forest + " house" + bcolors.ENDC +
+            ", and hide through the night. No creepers knocking on your door tonight! :D"
+        )
+        print(
+            bcolors.GREEN +
+            "\nYou've survived your first night of Mincecraft! Congratulations! Try again for a different ending."
+            + bcolors.ENDC)
 
     while respawn not in {"y", "n"}:
         respawn = input(bcolors.BLUE + '\nRespawn? (y/n): ' + bcolors.ENDC)
     if respawn == 'y':
         print("\033[H\033[J")
         gameCount = gameCount + 1
-        print(bcolors.PURPLE + "You've respawned " + str(gameCount) + " times" + bcolors.ENDC)
+        print(bcolors.PURPLE + "You've respawned " + str(gameCount) +
+              " times" + bcolors.ENDC)
     else:
         print('Thanks for playing!')
         break
